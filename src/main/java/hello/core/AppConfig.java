8ec0c0e13da71fb319b4b428e0b8ee6c24dcf24a
jiwon <kjw042626@gmail.com>
2025-03-17 17:16:09 +0900

    @컨피그레이션과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,18 +14,38 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+    //생각
+    //1. call AppConfig.memberService
+    //2. call AppConfig.memberRepository
+    //3. call AppConfig.memberRepository
+    //4. call AppConfig.orderService
+    //5. call AppConfig.memberRepository
+    // 순서는 다르겠지만 결과적으로 memberRepository 3번 호출됨
+
+    //실제
+    //1. call AppConfig.memberService
+    //2. call AppConfig.memberRepository
+    //3. call AppConfig.orderService
+    //스프링이 싱글톤을 보장해줌
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 

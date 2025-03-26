0fdc936f6b8408ea86c5229b6fd37aca52b4026b
jiwon <kjw042626@gmail.com>
2025-03-26 18:23:46 +0900

    빈 등록 초기화, 소멸 메소드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,17 +1,14 @@
 package hello.core.lifecycle;
 
+import lombok.Setter;
+
+@Setter
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 
     //서비스 시작시 호출
@@ -26,5 +23,16 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
